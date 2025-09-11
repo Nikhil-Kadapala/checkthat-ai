5be64b84bd3b434af4f34e29077036c6b8e5d0be
Nikhil Kadapala <nikhil.kadapala@unh.edu>
2025-09-11 03:58:50 -0500

    Remove all files related to the CheckThat AI Python SDK, including client, async client, types, exceptions, resources, and configuration files, effectively deprecating the older version of the SDK.

--- python sdk/__init__.py
+++
@@ -1,9 +0,0 @@
-from .client import CheckThatAI
-from .async_client import AsyncCheckThatAI
-from .version import __version__
-
-__all__ = [
-    "__version__",
-    "CheckThatAI",
-    "AsyncCheckThatAI",
-]
--- python sdk/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="checkthat-ai",
-    version="0.1.0",
-    packages=["checkthat_ai", "checkthat_ai.resources"],
-)
